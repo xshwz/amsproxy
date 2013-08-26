06f6ea67f4977c7b8c8b592b7d192026785f5b78
丘翔 <qiuxiang55aa@gmail.com>
2013-08-26 19:32:55 +0800

    修改站长统计代码

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -139,12 +139,8 @@
         <script src="js/bootstrap.min.js"></script>
         <script src="js/jquery.form.min.js"></script>
         <script src="js/amsProxy.js"></script>
-        <script>
-        var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-        document.write(unescape(
-            "%3Cdiv id='cnzz_stat_icon_1000039522'%3E%3C/div%3E%3Cscript src='" +
-            cnzz_protocol +
-            "s22.cnzz.com/z_stat.php%3Fid%3D1000039522'%3E%3C/script%3E"));
-        </script>
+
+        <div id="cnzz_stat_icon_1000039522"></div>
+        <script src="http://s22.cnzz.com/z_stat.php?id=1000039522"></script>
     </body>
 </html>
