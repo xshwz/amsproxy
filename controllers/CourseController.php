f2f477cdedb067726226ef31811907175dd91c1b
raidou <307196784@qq.com>
2013-09-01 23:42:12 +0800

    课表刷新

--- controllers/CourseController.php
+++ controllers/CourseController.php
@@ -6,4 +6,13 @@ class CourseController extends StudentController {
 	public function actionIndex() {
         $this->render('index', array('courses' => $this->getCourse()));
 	}
+
+    public function actionRefresh() {
+        $this->student->course = null;
+        $this->student->save();
+        $this->render('index', array(
+            'courses' => $this->getCourse(),
+            'refresh' => 1,
+        ));
+    }
 }
