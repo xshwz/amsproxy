c99442fe0a179efcfeb54316018afdaabb754b19
raidou <307196784@qq.com>
2013-09-04 00:19:37 +0800

    文字修改

--- views/site/index.php
+++ views/site/index.php
@@ -1,7 +1,7 @@
 <div class="jumbotron">
     <h1>Welcome!</h1>
     <?php if (defined('IS_LOGGED')):  ?>
-    <p>点击上面的菜单可以查看，学藉，课表，成绩等信息。</p>
+    <p>登录成功，点击上面的菜单可以查看，学藉，课表，成绩等信息。</p>
     <?php else: ?>
     <p>在这里，你可以查看课表和成绩</p>
     <a
