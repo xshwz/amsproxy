84442d8edfc197ceee4c0cec7a5ed0f090f557ac
丘翔 <qiuxiang55aa@gmail.com>
2013-10-11 10:28:54 +0800

    修改个人信息编辑，暂时隐藏

--- views/personal/archives.php
+++ views/personal/archives.php
@@ -9,80 +9,85 @@
         </div>
     </div>
     <?php endforeach; ?>
+
+    <!--
     <a href="#archives-edit" title="编辑" data-toggle="modal" class="btn archives-edit">
         <span class="glyphicon glyphicon-pencil"></span>
     </a>
+    -->
 
     <div class="modal fade" id="archives-edit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title"></h4>
+                    <h4 class="modal-title">编辑个人信息</h4>
                 </div>
                 <div class="modal-body">
                     <form class="form-horizontal" role="form" method="post">
                         <div class="form-group">
-                            <label for="phone" class="col-lg-2 control-label">联系电话</label>
-                            <div class="col-lg-10">
-                                <input name="txt0" type="text" class="form-control" id="phone" placeholder="联系电话"
+                            <label for="phone" class="col-sm-3 control-label">联系电话</label>
+                            <div class="col-sm-8">
+                                <input name="txt0" type="text" class="form-control" id="phone"
                                     <?php if (isset($studentInfo['联系电话'])) echo "value=\"{$studentInfo['联系电话']}\""; ?>>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="usedName" class="col-lg-2 control-label">曾用名</label>
-                            <div class="col-lg-10">
-                                <input name="txt1" type="text" class="form-control" id="usedName" placeholder="曾用名"
+                            <label for="usedName" class="col-sm-3 control-label">曾用名</label>
+                            <div class="col-sm-8">
+                                <input name="txt1" type="text" class="form-control" id="usedName"
                                     <?php if (isset($studentInfo['曾用名'])) echo "value=\"{$studentInfo['曾用名']}\""; ?>>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="hometown" class="col-lg-2 control-label">籍贯</label>
-                            <div class="col-lg-10">
+                            <label for="hometown" class="col-sm-3 control-label">籍贯</label>
+                            <div class="col-sm-8">
                                 <select name="txt2" class="form-control" id="hometown" 
                                     <?php if (isset($studentInfo['籍贯'])) echo "value=\"{$studentInfo['籍贯']}\""; ?>>
                                 </select>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="address" class="col-lg-2 control-label">联系地址</label>
-                            <div class="col-lg-10">
-                                <input name="txt3" type="text" class="form-control" id="address" placeholder="联系地址"
+                            <label for="address" class="col-sm-3 control-label">联系地址</label>
+                            <div class="col-sm-8">
+                                <input name="txt3" type="text" class="form-control" id="address"
                                     <?php if (isset($studentInfo['联系地址'])) echo "value=\"{$studentInfo['联系地址']}\"" ?>>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="contact" class="col-lg-2 control-label">联系人</label>
-                            <div class="col-lg-10">
-                                <input name="txt4" type="text" class="form-control" id="contact" placeholder="联系人"
+                            <label for="contact" class="col-sm-3 control-label">联系人</label>
+                            <div class="col-sm-8">
+                                <input name="txt4" type="text" class="form-control" id="contact"
                                     <?php if (isset($studentInfo['联系人'])) echo "value=\"{$studentInfo['联系人']}\"" ?>>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="zip-code" class="col-lg-2 control-label">邮政编码</label>
-                            <div class="col-lg-10">
-                                <input name="txt5" type="text" class="form-control" id="zip-code" placeholder="邮政编码"
+                            <label for="zip-code" class="col-sm-3 control-label">邮政编码</label>
+                            <div class="col-sm-8">
+                                <input name="txt5" type="text" class="form-control" id="zip-code"
                                     <?php if (isset($studentInfo['邮政编码'])) echo "value=\"{$studentInfo['邮政编码']}\"" ?>>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="political-landscape" class="col-lg-2 control-label">政治面貌</label>
-                            <div class="col-lg-10">
+                            <label for="political-landscape" class="col-sm-3 control-label">政治面貌</label>
+                            <div class="col-sm-8">
                                 <select name="txt6" class="form-control" id="political-landscape"
                                     <?php if (isset($studentInfo['政治面貌'])) echo "value=\"{$studentInfo['政治面貌']}\"" ?>>
                                 </select>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="cell-phone" class="col-lg-2 control-label">手机号码</label>
-                            <div class="col-lg-10">
-                                <input name="txt7" type="text" class="form-control" id="cell-phone" placeholder="手机号码"
+                            <label for="cell-phone" class="col-sm-3 control-label">手机号码</label>
+                            <div class="col-sm-8">
+                                <input name="txt7" type="text" class="form-control" id="cell-phone"
                                     <?php if (isset($studentInfo['手机号码'])) echo "value=\"{$studentInfo['手机号码']}\"" ?>>
                                 </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-lg-offset-2 col-lg-10">
-                                <button type="submit" class="btn btn-default">修改</button>
+                            <div class="col-sm-offset-3 col-sm-9">
+                                <button type="submit" class="btn">
+                                    <i class="glyphicon glyphicon-ok"></i> 提交
+                                </button>
                             </div>
                         </div>
                     </form>
