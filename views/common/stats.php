0d0ecfd63f52cb55689c151d5bdaa40305482b99
丘翔 <qiuxiang55aa@gmail.com>
2013-10-26 17:25:48 +0800

    重新组织统计代码；尝试百度统计

---
+++ views/common/stats.php
@@ -0,0 +1,17 @@
+<div id="cnzz_stat_icon_1000039522"></div>
+<script src="http://s22.cnzz.com/z_stat.php?id=1000039522"></script>
+
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-43494312-1', 'gxun.edu.cn');
+ga('send', 'pageview');
+</script>
+
+<script>
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fa2186fb065286f8785f3b01b2afeeea6' type='text/javascript'%3E%3C/script%3E"));
+</script>
